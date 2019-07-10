c278c4cead4632c7d988fef8feee608cc4e1c916
xiewenlong1202 <xiewenlong1202@gmail.com>
2019-07-10 16:32:06 +0800

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -75,7 +75,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # She notices that her list has a unique URL
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/list/.+')
+        self.assertRegex(edith_list_url, '/lists/.+')
 
         # Now a new user, Francis, comes along to the site
 
